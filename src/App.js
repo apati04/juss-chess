be465d6a296eda8b54f091a41a52a10de9f91c57
Andrew Patipaksiri <apax714@gmail.com>
2017-10-01 13:25:09 -0700

    app component skeleton

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { HashRouter, Route } from 'react-router-dom';
+
 import './App.css';
+// import Home from './Home';
+// import Game from './Game';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return(
+      <HashRouter>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Game}/>
+      </HashRouter>
+    )
   }
 }
 
